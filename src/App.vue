3656679c097f3f020a58caf0083596dcc4b61ee2
Hrva <harwey.ch@gmail.com>
2021-08-02 20:46:03 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-for="cat in cats" :key="cat.id">
+      <h1>{{ cat.id }}</h1>
+      <p>{{ cat.name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import cats from "./assets/cat.json";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      cats: cats,
+    };
+  },
+  created() {},
+};
 </script>
 
 <style>
